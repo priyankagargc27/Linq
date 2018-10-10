32e463573f23668fca533072ee7c72420b08f5d1
priyankagargc27 <40770470+priyankagargc27@users.noreply.github.com>
2018-10-10 16:16:37 -0500

    doing some more

--- Program.cs
+++ Program.cs
@@ -55,6 +55,58 @@ foreach(string n in des)
 {
     Console.WriteLine(n);
 }   
- }
+
+// Build a collection of these numbers sorted in ascending order
+// Output how many numbers are in this list
+List<int> Numbers = new List<int>()
+{
+    15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+};
+var asc=
+from num in Numbers
+orderby num ascending
+select num;
+Console.WriteLine("Numbers in ascending order are");
+foreach(int num in asc)
+{
+    Console.WriteLine(num);
+}
+int CountNumber = Numbers.Count(); 
+  
+    Console.WriteLine($"There are {CountNumber} number"); 
+// How much money have we made?
+List<double> purchases = new List<double>()
+{
+    2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+};
+
+double SumNumber = purchases.Sum(); 
+  
+    Console.WriteLine($"Total Money is {SumNumber}"); 
+    // What is our most expensive product?
+List<double> prices = new List<double>()
+{
+    879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+};
+Console.WriteLine($"most expensive product is {prices.Max()}");
+
+/*
+    Store each number in the following List until a perfect square
+    is detected.
+
+    Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+*/
+List<int> wheresSquaredo = new List<int>()
+{
+    66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+};  
+ List<int> nonSquares = wheresSquaredo.TakeWhile(n => Math.Sqrt(n) % 1 != 0).ToList();
+            foreach (var item in nonSquares)
+            {
+                Console.WriteLine(item);
+            }
     }
 }
+}
+
+
